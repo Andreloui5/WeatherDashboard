1cc05f02f639da16360eb3f22fba87d2e7bea689
Craig Gant <craigkgant@gmail.com>
2019-12-12 22:42:33 -0500

    added framework card and function for 5day forcast

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,7 +10,7 @@ function currentWeather(searchValue){
         url: weatherUrl + searchValue + weatherApiKey,
         type: "GET",
     }).then(function(response){
-        console.log(response)
+        // console.log(response)
         //adds a class of card to the main dashboard (enabling the light border)
         var dashboard = $("#mainDashboard").addClass("card");
         // Adds the title to the page: '____' weather details:
@@ -24,6 +24,29 @@ function currentWeather(searchValue){
         //puts today's humidity on the page
         var todaysHumid = $("#todaysHumid").text("Humidity: " + response.main.humidity + "%");
         getUV(response.coord.lat, response.coord.lon)
+        fiveDayForcast(response.coord.lat, response.coord.lon);
+    });
+
+}
+
+function fiveDayForcast(lat, lon){
+    $.ajax({
+        url:  "http://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lat + "&lon=" + lon + weatherApiKey,
+        type: "GET",
+    }).then(function(response){
+        console.log(response)
+        //adds a class of card to the main dashboard (enabling the light border)
+        // var dashboard = $("#mainDashboard").addClass("card");
+        // // Adds the title to the page: '____' weather details:
+        // var title = $(".today").html(response.name + " weather details:");
+        // //puts today's date on the page
+        // var currentDate = $("#currentDate").text(moment().format("l"));
+        // //puts today's temperature on the page
+        // var todaysTemp = $("#todaysTemp").text("Temperature: " + response.main.temp + "°F");
+        // //puts today's 'feels like' temperature on the page
+        // var todaysFeels = $("#todaysFeels").text("Feels like: " + response.main.feels_like + "°F");
+        // //puts today's humidity on the page
+        // var todaysHumid = $("#todaysHumid").text("Humidity: " + response.main.humidity + "%");
     });
 
 }
@@ -34,7 +57,7 @@ function getUV(lat, lon){
         url: "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lat + "&lon=" + lon + weatherApiKey,
         type: "GET",
     }).then(function(response){
-        console.log(response);
+        // console.log(response);
         //adds text stating the current UV Index at the chosen location
         var uvStaticText = $(".uv").text("UV index:");
         var uvNum = $(".uvNum").text(response.value)
@@ -58,15 +81,23 @@ function getUV(lat, lon){
 
 }
 
+// Make jumbotron initial search button (it searches and then collapses/empties everything)
+// $("#initial-search-button").on("click", function(){
+//     //makes variable for the value the user inputs into the search area and trims it
+//     var searchValue = $("#initial-search-value").val()
+//     $("#startScreen").remove();
+//     $("#mainPage").removeClass("collapse");
+//     //searches openweather for:
+//     currentWeather(searchValue)
+// })
+
 //When search button is clicked, this records that data
 $("#search-button").on("click", function(){
     //makes variable for the value the user inputs into the search area and trims it
     var searchValue = $("#search-value").val().trim()
-
     //searches openweather for:
     currentWeather(searchValue)
 })
-
 
 
 
